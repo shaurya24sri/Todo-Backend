3d1869f0a17344365a1d1fb8743cbd7af2f6b6bf
Shaurya Srivastava <81847195+shaurya24sri@users.noreply.github.com>
2022-04-07 12:44:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Connecting the Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zybrdrdsgxvtss:bd83da69e431203e28acfe61ffcdd891b303914f3cd6005058fff79f19a5abd6@ec2-54-173-77-184.compute-1.amazonaws.com:5432/d5pn062kkjdnfu'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://andundolhnmnbz:698b885c14aa091055627a2571cbcb7d9c40fad258501d5c88fe97fa9773defa@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d6odcav599l1m2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False         
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
